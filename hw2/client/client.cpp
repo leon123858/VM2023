7c119bc25d611117a1654a6bbd9165eb140dbe02
leon123858 <a0970785699@gmail.com>
2023-03-29 19:58:52 +0000

    get target kvm success

--- hw2/client/client.cpp
+++ hw2/client/client.cpp
@@ -12,18 +12,17 @@
 using namespace std;
 
 struct kvm_arm_write_gpa_args {
-       uint32_t vmid;  // the vmid that you, as the host, want to write to
+       uint32_t vmid;  // the vmid that you, as the host, want to write to (default 1)
        uint64_t gpa;   // the gpa(guest physical address) of the guest
        uint64_t data;  // address of the payload in host user space
        uint64_t size;  // size of the payload
 };
 #define KVMIO 0xAE
-#define KVM_GET_API_VERSION       _IO(KVMIO,   0x00)
 #define KVM_ARM_WRITE_GPA         _IOW(KVMIO,   0xfe, struct kvm_arm_write_gpa_args)
  
 int main()
 {
-        struct kvm_arm_write_gpa_args tmp = {10,5,8,0};
+        struct kvm_arm_write_gpa_args tmp = {0,55688,2266,1};
         int fd;
         int32_t value, number;
         printf("*********************************\n");
@@ -38,7 +37,6 @@ int main()
         if(result == -1){
                 printf("errON[%d]errMsg[%s]\n",errno,strerror(errno));
         }
-        // ioctl(fd, RD_VALUE, (int32_t*) &value);
         printf("Value is %d\n", result);
         printf("Closing Driver\n");
         close(fd);
