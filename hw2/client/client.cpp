5b994e596a884b5b68680f949e884ae547cb3794
leon123858 <a0970785699@gmail.com>
2023-03-29 10:46:33 +0000

    ioctl OK

---
+++ hw2/client/client.cpp
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <cstdint>
+#include <sys/ioctl.h>
+#include <errno.h>
+
+using namespace std;
+
+struct kvm_arm_write_gpa_args {
+       uint32_t vmid;  // the vmid that you, as the host, want to write to
+       uint64_t gpa;   // the gpa(guest physical address) of the guest
+       uint64_t data;  // address of the payload in host user space
+       uint64_t size;  // size of the payload
+};
+#define KVMIO 0xAE
+#define KVM_GET_API_VERSION       _IO(KVMIO,   0x00)
+#define KVM_ARM_WRITE_GPA         _IOW(KVMIO,   0xfe, struct kvm_arm_write_gpa_args)
+ 
+int main()
+{
+        struct kvm_arm_write_gpa_args tmp = {10,5,8,0};
+        int fd;
+        int32_t value, number;
+        printf("*********************************\n");
+ 
+        printf("\nOpening Driver\n");
+        fd = open("/dev/kvm", O_RDWR);
+        if(fd < 0) {
+                printf("Cannot open device file...\n");
+                return 0;
+        }
+        int result = ioctl(fd, KVM_ARM_WRITE_GPA, &tmp); 
+        if(result == -1){
+                printf("errON[%d]errMsg[%s]\n",errno,strerror(errno));
+        }
+        // ioctl(fd, RD_VALUE, (int32_t*) &value);
+        printf("Value is %d\n", result);
+        printf("Closing Driver\n");
+        close(fd);
+}
